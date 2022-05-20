369ee64cbad6477545a5adf11664e2d20616eff6
pedrocarneiro11 <pedrocarneirocunha87@gmail.com>
2022-05-20 16:34:41 -0300

    Adicionando arquivos .cpp e .exe

---
+++ Algoritmosoma.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+main(){
+	int a, b, soma;
+	cout << "Digite a: ";
+	cin >> a;
+	cout << "Digite b: ";
+	cin >> b;
+	soma = a+b;
+	printf("O resultado da soma e = %d\n",soma);
+	system("pause");
+	return(0);
+}
+
